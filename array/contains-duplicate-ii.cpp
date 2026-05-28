4a61fcf7ff92838174ae97a5716ae87c635409dc
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-05-28 17:06:12 +0530

    LC-contains-duplicate-ii [Runtime: N/A; Memory: N/A]

--- array/contains-duplicate-ii.cpp
+++ array/contains-duplicate-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& arr, int k) {
+        unordered_map<int,int>mp;
+        
         int n  = arr.size();
-        
-        
-        for(int i =0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-            if(arr[i]==arr[j] && i!=j && abs(i-j)<=k){
-                return true;
+        for(int i = 0;i<n;i++){
+            if(mp.find[nums[i]]!= mp.end()){
+                if(i - mp[nums[i]]<=k){
+                    return true;
+                }
             }
-            
+            mp[nums[i]] = i;
         }
+
         
-    }
+        
+    
      return false;
     }
    
