b319cf9fe83f4fcaf450568ee6a4b5344e694ac9
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-05-29 21:16:40 +0530

    LC-majority-element-ii [Runtime: N/A; Memory: N/A]

--- hash-table/majority-element-ii.cpp
+++ hash-table/majority-element-ii.cpp
@@ -1,46 +1,41 @@
 class Solution {
-public:
-    vector<int> majorityElement(vector<int>& v) {
-       int  n = v.size();
-        int el1=0,el2=0;
-        int cnt1 =0,cnt2=0;
-        for(int i = 0;i<n;i++)
-        {
-            if(el1==v[i])
-            {
+  public:
+    vector<int> findMajority(vector<int>& v) {
+        // Code here
+        int n  = v.size();
+        int el1 = 0,el2 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i =0;i<n;i++){
+            if(el1==v[i]){
                 cnt1++;
             }
-            else if(el2==v[i])
-            {
+            else if(el2==v[i]){
                 cnt2++;
+                
             }
-            else if(cnt1==0)
-            {
+            else if(cnt1==0){
                 el1 = v[i];
-                cnt1=1;
+                cnt1 = 1;
             }
-            else if(cnt2==0)
-            {
+            else if(cnt2==0){
                 el2 = v[i];
-                cnt2 = 1;
+                cnt2=1;
             }
             else{
-                cnt1--;
+                cnt1;
                 cnt2--;
             }
         }
-     
-        cnt1 = cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el1) cnt1++;
-            else if (v[i] == el2) cnt2++;
+        cnt1=cnt2= 0;
+        for(int i = 0;i<n;i++){
+            if(el1 == v[i]){
+                cnt1++;
+            }
+            else if(v[i]==el2)cnt2++;
         }
-
-        vector<int> ans;
-        if (cnt1 > n/3) ans.push_back(el1);
-        if (cnt2 > n/3) ans.push_back(el2);
-
-        return ans;
-    
+        vector<int>ans;
+        if(cnt1>n/3)ans.push_back(el1);
+        if(cnt2>n/3)ans.push_back(el2);
     }
 };
