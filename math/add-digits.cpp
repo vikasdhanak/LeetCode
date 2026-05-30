7609aa4664b24675fec6bbb8b665861f9ed38c27
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-05-30 19:50:27 +0530

    LC-add-digits [Runtime: N/A; Memory: N/A]

--- math/add-digits.cpp
+++ math/add-digits.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-         if(num==0)return 0;
-         if(num%9 == 0)return 9;
+         if(num==0)return 0; // Zero case: If num is 0, digital root is 0
+         if(num%9 == 0)return 9;//If num % 9 equals 0 (and num ≠ 0), digital root is 9
 
-         return num%9;
+         return num%9; // For all other numbers, digital root is num % 9
         
       
     }
