906013d7609e417d28f181a60bb163903930c21b
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-02 10:03:09 +0530

    LC-concatenation-of-array [Runtime: N/A; Memory: N/A]

--- array/concatenation-of-array.cpp
+++ array/concatenation-of-array.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        nums.insert(nums.end(),nums.begin(),nums.end());
-        return nums;
+        int n  = nums.size();
+        vector<int>ans(2*n);
+       
+        for(int i = 0;i<n;i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+ 
     }
 };
