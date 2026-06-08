8698d67455734e0c476b69eee19a6ff4ae3866c9
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-08 13:04:44 +0530

    LC-detect-capital [Runtime: N/A; Memory: N/A]

--- string/detect-capital.cpp
+++ string/detect-capital.cpp
@@ -10,7 +10,7 @@ public:
         int n = word.size();
         if(countcapital==n)return true; //  all are capital it means
         if(countcapital==0)return true; //  no work is capital that means
-        if(countcapital==1)return true; // first leter is capital it means
+        if(countcapital==1 && isupper(word[0]))return true; // first leter is capital it means
 
 
         return false;
