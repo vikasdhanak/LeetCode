c32afaaf5cfc3e83d88e3091e5b1144e1ad48cf3
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-16 10:00:57 +0530

    LC-maximum-total-subarray-value-i [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-total-subarray-value-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        int mx = arr[0];
+        int mn = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > mx) {
+                mx = arr[i];
+            }
+            if (arr[i] < mn) {
+                mn = arr[i];
+            }
+        }
+
+        int subarray = mx - mn;
+        return subarray * k;
+    }
+};
