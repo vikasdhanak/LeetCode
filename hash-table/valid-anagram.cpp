ee35810b72d083ca93cb8ce19b2b62dca4f8cb30
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-19 11:58:19 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.cpp
+++ hash-table/valid-anagram.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string txt, string pat) {
-        sort(txt.begin(), txt.end());
-        sort(pat.begin(), pat.end());
-        if(txt.size()!=pat.size()) return false;
-
-        for(int i = 0;i<txt.size();i++){
-            if(txt[i]!=pat[i]){
-                return false;
-            }
-            
+        unordered_map<char,int>mp;
+        if (txt.size() != pat.size()) return false;
+        for(int i =0;i<pat.size();i++){
+            mp[pat[i]]++;
         }
-    return true;
+        for(auto it:txt){
+            if(mp.find(it)!=mp.end()){
+                mp[it]--;
+                if(mp[it]==0){
+                    mp.erase(it);
+                }
+                
+            }
+            else{
+                    return false;
+            }
+        }
+        return mp.empty();
     }
 };
 
