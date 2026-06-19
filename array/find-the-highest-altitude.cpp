d698b950b378fcb44e4cd5cffccd4a5694b036ea
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-19 08:35:32 +0530

    LC-find-the-highest-altitude [Runtime: N/A; Memory: N/A]

--- array/find-the-highest-altitude.cpp
+++ array/find-the-highest-altitude.cpp
@@ -4,16 +4,13 @@ public:
         int n = gain.size();
         vector<int>prefixsum(n+1);
         prefixsum[0] = 0;
+        int maxheight = 0;
         for(int i = 1;i<=n;i++){
             prefixsum[i] = prefixsum[i-1] + gain[i-1];
+            if(prefixsum[i]>maxheight) maxheight = prefixsum[i];
         }
-        int max  = prefixsum[0];
-        for(int i = 1; i<prefixsum.size();i++){
-            if(prefixsum[i]>max){
-                max = prefixsum[i];
-            }
-        }
-        return max;
+        
+        return maxheight;
 
         
 
