5e462564fd93455f93f6900d1a0808d29f18d0df
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-26 17:36:27 +0530

    LC-remove-linked-list-elements [Runtime: N/A; Memory: N/A]

---
+++ linked-list/remove-linked-list-elements.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL)return NULL;
+
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* curr = dummy;
+        while(curr->next!=NULL){
+            if(curr->next->val == val){
+                curr->next = curr->next->next;
+            }
+            else{
+                curr = curr->next;
+            }
+            
+        }
+        return dummy->next;
+    }
+};
