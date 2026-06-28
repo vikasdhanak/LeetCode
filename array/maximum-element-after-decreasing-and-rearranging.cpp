4ba49312db54612dde9392f51b345630f42551fa
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-28 09:17:14 +0530

    LC-maximum-element-after-decreasing-and-rearranging [Runtime: N/A; Memory: N/A]

--- array/maximum-element-after-decreasing-and-rearranging.cpp
+++ array/maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,18 +3,14 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         // greedy approach 
 
-        int maximum = INT_MIN;
+         
         sort(arr.begin(),arr.end());
-        for(int i = 0;i<arr.size();i++){
-            if(i==0){
-                arr[i] = 1;
-            }
-            else if(abs(arr[i] - arr[i-1])>=1){
-                arr[i] = arr[i-1]+1;
-                maximum = max(maximum , arr[i]);
-            }
+        arr[0] = 1;
+        for(int i = 1;i<arr.size();i++){
+            arr[i] = min(arr[i] , arr[i-1]+1);
+
         }
-        return maximum;
+        return arr.back();
 
     }
 };
