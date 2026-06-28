41874024839dc2b406cc83ae9bfc4c4f4d050745
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-28 13:09:53 +0530

    LC-palindrome-linked-list [Runtime: N/A; Memory: N/A]

--- two-pointers/palindrome-linked-list.cpp
+++ two-pointers/palindrome-linked-list.cpp
@@ -12,16 +12,52 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
 
-        if(head==NULL)return false;
-        if(head->next==NULL)return true;
-        ListNode* temp = head;
-        ListNode* tail = head;
-        while(tail->next){
-            tail = tail->next;
-            
-        }
-        if(temp->val != tail->val)return false;
+        if(head->next== NULL)return 1;
 
-        return true;
+        // counting the nu of nodes
+        int count = 0;
+        ListNode* temp = head;
+        while(temp){
+            count++;
+            temp = temp->next;
+        }
+        count = count/2;
+
+        // breaking the linked list into two halves 
+        ListNode* curr = head;
+        ListNode* prev= NULL;
+        while(count--){
+            prev = curr;
+            curr= curr->next;
+        }
+        prev->next = NULL; // breaking it
+
+        // reverse the second linked list , curr is pointing it
+        ListNode* front;
+        prev= NULL;
+        
+        while(curr){
+            front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr= front;
+        }
+        // prev is pointing to second list
+        // head is pointing to first list
+
+        ListNode* head1 = head;
+        ListNode* head2 = prev;
+        // check pallindrome
+        while(head1){
+            if(head1->val != head2->val)return 0;
+            head1 = head1->next;
+            head2 = head2->next;
+
+
+        }
+        return 1;
+
+
+    
     }
 };
