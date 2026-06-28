d08fcc44248fa947806e768b106cc56c7bbced70
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-28 12:06:30 +0530

    LC-rotate-list [Runtime: N/A; Memory: N/A]

--- two-pointers/rotate-list.cpp
+++ two-pointers/rotate-list.cpp
@@ -11,28 +11,66 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        vector<int>ans;
-        ListNode* temp = head;
-        if(head == NULL)return head;
+        // vector<int>ans;
+        // ListNode* temp = head;
+        // if(head == NULL)return head;
+        // while(temp){
+        //     ans.push_back(temp->val);
+        //     temp = temp->next;
+            
+        // }
+        // int n = ans.size();
+        // int d = k%n;
+        // reverse(ans.begin(),ans.begin() + n-d);
+        // reverse(ans.begin()+ n-d, ans.end());
+        // reverse(ans.begin() ,ans.end());
+
+        // int i = 0;
+        // ListNode* temp1 = head;
+
+        // while(temp1){
+        //     temp1->val = ans[i++];
+            
+        //     temp1 = temp1->next;
+        // }
+        // return head;
+
+        // NEW APPROACH ACTUALLY ROTATING NODES BY MAKING IT A CIRCULAR LIST
+
+        if(head==NULL || head->next == NULL)return head;
+        
+
+        int count = 0;
+        ListNode* temp= head;
         while(temp){
-            ans.push_back(temp->val);
-            temp = temp->next;
-            
+            count++;
+            temp  = temp->next;
         }
-        int n = ans.size();
-        int d = k%n;
-        reverse(ans.begin(),ans.begin() + n-d);
-        reverse(ans.begin()+ n-d, ans.end());
-        reverse(ans.begin() ,ans.end());
+        k = k%count;
+        if(k==0)return head;
 
-        int i = 0;
-        ListNode* temp1 = head;
+        count -=k;
+        
+        if(count==0)return head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while(count--){
+            prev = curr;
+            curr = curr->next;
 
-        while(temp1){
-            temp1->val = ans[i++];
-            
-            temp1 = temp1->next;
         }
-        return head;
+        prev->next = NULL;  // connection broke
+
+        ListNode* tail  = curr;
+        while(tail->next){
+            tail = tail->next;
+        }
+        tail->next = head;
+
+        head  = curr;
+        return head; // curr is the head poiinter
+
+        
+
     }
 };
