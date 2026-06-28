0f6ac47c199bbb863a7e2a417d5a727303047255
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-06-28 09:09:02 +0530

    LC-maximum-element-after-decreasing-and-rearranging [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        // greedy approach 
+
+        int maximum = INT_MIN;
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i<arr.size();i++){
+            if(i==0){
+                arr[i] = 1;
+            }
+            else if((arr[i] - arr[i-1])>=1){
+                arr[i] = arr[i-1]+1;
+                maximum = max(maximum , arr[i]);
+            }
+        }
+        return maximum;
+
+    }
+};
