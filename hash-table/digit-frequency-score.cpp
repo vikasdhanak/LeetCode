90b1db30c22a6eedddbc82be6d8e7ba8ecaa89d4
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-02 23:29:07 +0530

    LC-digit-frequency-score [Runtime: N/A; Memory: N/A]

--- hash-table/digit-frequency-score.cpp
+++ hash-table/digit-frequency-score.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     int digitFrequencyScore(int n) {
         int ans= 0;
+        unordered_map<int,int>mp;
         while(n>0){
-            int rem  = n%10;
-            ans += rem;
-            n = n/10;
+            int rem = n%10;
+            mp[rem]++;
+            n=n/10;
+        }
+        for(auto it:mp){
+            ans += (it.second * it.first);
         }
         return ans;
     }
