b7cd95fd4b066e75b251141523a5f995ed79a385
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-03 10:33:46 +0530

    LC-count-the-number-of-consistent-strings [Runtime: N/A; Memory: N/A]

---
+++ string/count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+
+        unordered_set<char> st(allowed.begin(), allowed.end());
+        for (int i = 0; i < words.size(); i++) {
+
+            bool valid = true;
+
+            for (int j = 0; j < words[i].size(); j++) {
+
+                if (st.find(words[i][j]) == st.end()) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+                ans++;
+        }
+        return ans;
+    }
+};
