b3c2c44ee19bc4a0a127a2aa125ccc14de3dfdf4
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-03 10:34:12 +0530

    LC-count-the-number-of-consistent-strings [Runtime: N/A; Memory: N/A]

--- hash-table/count-the-number-of-consistent-strings.cpp
+++ hash-table/count-the-number-of-consistent-strings.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
 
+        int ans = 0;
         unordered_set<char> st(allowed.begin(), allowed.end());
         for (int i = 0; i < words.size(); i++) {
 
