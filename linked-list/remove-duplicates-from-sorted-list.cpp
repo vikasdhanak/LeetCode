fa63bdfe718fad9d04212cf51d3112257da471af
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-03 16:21:40 +0530

    LC-remove-duplicates-from-sorted-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
+        while (curr!=NULL & curr->next!=NULL) {
+            prev = curr;
+            curr = curr->next;
+
+            if (prev->val == curr->val) {
+                prev->next = curr->next;
+            }
+            
+        }
+        return head;
+    }
+};
