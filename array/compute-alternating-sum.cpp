80d0f324794ac486e043621ade1b91564ecd3db1
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-04 08:08:26 +0530

    LC-compute-alternating-sum [Runtime: N/A; Memory: N/A]

---
+++ array/compute-alternating-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sum = sum + nums[i];
+            } else {
+                sum = sum - nums[i];
+            }
+        }
+        return sum;
+    }
+};
