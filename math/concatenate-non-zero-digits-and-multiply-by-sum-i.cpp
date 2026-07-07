992c3504f0c3e3b133dd18bf7697cb5e97b74cab
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-07 10:21:41 +0530

    LC-concatenate-non-zero-digits-and-multiply-by-sum-i [Runtime: N/A; Memory: N/A]

---
+++ math/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        int temp = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                temp = temp * 10 + digit;
+            }
+            n = n / 10;
+        }
+        int ans = 0;
+        while (temp > 0) {
+            ans = ans * 10 + (temp % 10);
+            temp = temp / 10;
+        }
+        int num = ans;
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+
+        long long final = ans * sum;
+        return final;
+    }
+};
