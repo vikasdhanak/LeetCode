3f8531e21cc3f8abb88ccbd121b3f5c090af85bf
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-20 22:43:45 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.cpp
+++ array/two-sum.cpp
@@ -1,16 +1,14 @@
- class Solution {
+class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
-            mp[nums[i]] = i;
         }
-
-        return {};
+        return {}; 
     }
 };
