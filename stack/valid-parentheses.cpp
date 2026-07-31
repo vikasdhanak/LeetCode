0c4dca38fc43611f8612b9050db18f3e297d255c
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-07-31 22:40:03 +0530

    LC-valid-parentheses [Runtime: 0 ms; Memory: 8.9 MB]

--- stack/valid-parentheses.cpp
+++ stack/valid-parentheses.cpp
@@ -1,28 +1,25 @@
- class Solution {
+ #include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for(char c : s) {
-            
-            if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            }
-            else {
-               
-                if(st.empty()) return false;
-
-                char top = st.top();
-                if((c == ')' && top == '(') ||
-                   (c == '}' && top == '{') ||
-                   (c == ']' && top == '[')) {
-                    st.pop();
-                } else {
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
                     return false;
                 }
+                char top = st.top();
+                st.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
             }
         }
-
-        return st.empty();  
+        return st.empty();
     }
 };
