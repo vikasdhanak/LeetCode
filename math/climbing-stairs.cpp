533f4542b4f5ce590be4beb1249a98fcbeec0408
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-08-02 11:56:33 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.cpp
+++ math/climbing-stairs.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=2)return n;
+        // if(n<=1)return 1; // base case
+
+        // return climbStairs(n-1) + climbStairs(n-2);
+
+        // same as fibonaaci seties just start from base input like 1,2,3,4..
+        // and analyze the pattern
+
+        // the ans is its sum of its 2 previous  no
+
+        // for now it is showing time limit exceeded but i will come back and
+        // asolve it using dp
 
 
-        return climbStairs(n-1) + climbStairs(n-2);
+        if(n==1)return 1;
+
+        int prev2= 1;
+        int prev1 = 2;
+        
+        for (int i = 3; i <=n; i++) {
+          
+            int current = prev1+prev2;
+            prev2 = prev1;
+
+            prev1 = current;
+            
+        }
+        return prev1;
     }
 };
