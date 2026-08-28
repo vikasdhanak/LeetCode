154a01c29463ab4caa98b8547afc7e0db8af4e53
vikas_dhanak_18 <VIKASDHANAK.240111405@gehu.ac.in>
2026-08-28 15:24:00 +0530

    LC-sort-an-array [Runtime: N/A; Memory: N/A]

--- radix-sort/sort-an-array.cpp
+++ radix-sort/sort-an-array.cpp
@@ -1,8 +1,62 @@
 class Solution {
 public:
+    void merge(vector<int>&arr, int start, int mid, int end) {
+        vector<int> temp(end - start + 1);
+
+        int left = start, right = mid + 1, index = 0;
+
+        while (left <= mid && right <= end) {
+            if (arr[left] <= arr[right]) {
+                temp[index] = arr[left];
+                index++;
+                left++;
+            } else {
+                temp[index] = arr[right];
+                index++;
+                right++;
+            }
+        }
+        // for the remaing element after boundaries reach
+        // left array agar elemnt bacha h to unko direct copy krdo
+        while (left <= mid) {
+
+            temp[index] = arr[left];
+            index++;
+            left++;
+        }
+
+        // for right elment
+        while (right <= end) {
+
+            temp[index] = arr[right];
+            index++;
+            right++;
+        }
+
+        // filling temp elements to array
+
+        index = 0;
+        while (start <= end) {
+            arr[start] = temp[index];
+            start++, index++;
+        }
+    }
+    void mergesort(vector<int>& arr, int start, int end) {
+        if (start >= end)
+            return; // base case
+
+        int mid = start + (end - start) / 2;
+
+        mergesort(arr, start, mid);   // left side
+        mergesort(arr, mid + 1, end); // right side
+
+        // for merging
+        merge(arr, start, mid, end);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        
-        sort(nums.begin() , nums.end()) ;
+
+        mergesort(nums, 0, nums.size() - 1);
+
         return nums;
     }
 };
